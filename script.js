a7deef693e5a21eb8f704e7f4929f37d1ecd02d1
Philip Ifeanyi <akubuiro.ifeanyi.philip@gmail.com>
2022-04-16 03:21:38 +0100

    worked on the contact form JS

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ function logSubmit(event) {
 
 const form = document.getElementById('form');
 form.addEventListener('submit', logSubmit);
+
+const contactForm = document.getElementById('contact-form');
+
+contactForm.addEventListener('submit', (event) => {
+  prompt('Thanks for Registering With Us, Check Your Email for a Confirmation Mail');
+  event.preventDefault();
+});
